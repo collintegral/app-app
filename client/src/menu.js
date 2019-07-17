629471a779ed39f7a430de2204877d36cc19ebd8
jsmithwh <46731513+Jsmithwh@users.noreply.github.com>
2019-07-16 21:24:02 -0500

    added additional functionality to menu

--- client/src/menu.js
+++ client/src/menu.js
@@ -1,98 +1,102 @@
+import './menu.css';
+
 function App() {
     return (
-        <aside class="menu">
-            <p class="menu-label">
-                Components
-        </p>
-            <ul class="menu-list">
-                <li><a id="card" draggable="true" ondragstart="drag(event)">Card</a>
-                    <div class="card">
-                        <div class="card-content">
-                            <p class="title">
-                                Card
-                            </p>
-                        </div>
-                        <footer class="card-footer">
-                            <p class="card-footer-item">
-                                <span>
-                                    View on <a href="">Twitter</a>
-                                </span>
-                            </p>
-                            <p class="card-footer-item">
-                                <span>
-                                    Share on <a href="#">Facebook</a>
-                                </span>
-                            </p>
-                        </footer>
-                    </div>
-                </li>
-                <li><a id="navBar" draggable="true" ondragstart="drag(event)">Navbar</a>
-                    <nav class="navbar" role="navigation" aria-label="main navigation">
-                        <div class="navbar-brand">
+        <aside className="menu is-pulled-left">
+    <p className="menu-label">
+    <p className="menu-label">
+        Components
+</p>
+    <ul classNameName="menu-list">
+        <li><a id="card" draggable="true" ondragstart="drag(event)">Card</a>
+            <div className="card" draggable="true" ondragstart="drag(event)">
+                <div className="card-content">
+                    <p className="title">
+                        Card
+                    </p>
+                </div>
+                <footer className="card-footer">
+                    <p className="card-footer-item">
+                        <span>
+                            View on <a href="">Twitter</a>
+                        </span>
+                    </p>
+                    <p className="card-footer-item">
+                        <span>
+                            Share on <a href="#">Facebook</a>
+                        </span>
+                    </p>
+                </footer>
+            </div>
+        </li>
+        <li><a id="navBar" draggable="true" ondragstart="drag(event)">Navbar</a>
+            <nav className="navbar"  draggable="true" ondragstart="drag(event)" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
 
-                            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-                                <span aria-hidden="true"></span>
-                                <span aria-hidden="true"></span>
-                                <span aria-hidden="true"></span>
-                            </a>
-                        </div>
-                    </nav>
-                </li>
-                <li><a id="message" draggable="true" ondragstart="drag(event)">Message</a>
-                    <article class="message">
-                        <div class="message-header">
-                            <p>Message</p>
-                            <button class="delete" aria-label="delete"></button>
-                        </div>
-                        <div class="message-body">
-                            Message Body
-                        </div>
-                    </article>
-                </li>
-                <li><a id="imageBox" draggable="true" ondragstart="drag(event)">Image Box</a>
-                    <figure class="image is-128x128">
-                        <img src=""></img>
-                    </figure>
-                </li>
-                <li><a id="hero" draggable="true" ondragstart="drag(event)">Hero</a>
-                    <section class="hero">
-                        <div class="hero-body">
-                            <div class="container">
-                                <h1 class="title">
-                                    Hero title
-                                </h1>
-                                <h2 class="subtitle">
-                                    Hero subtitle
-                                </h2>
-                            </div>
-                        </div>
-                    </section>
-                </li>
-                <li><a id="form" draggable="true" ondragstart="drag(event)">Form</a>
-                    <div class="field">
-                        <label class="label">Name</label>
-                        <div class="control">
-                            <input class="input" type="text" placeholder="e.g Alex Smith"></input>
-                        </div>
+                    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+            </nav>
+        </li>
+        <li><a id="message" draggable="true" ondragstart="drag(event)">Message</a>
+            <article className="message"  draggable="true" ondragstart="drag(event)">
+                <div className="message-header">
+                    <p>Message</p>
+                    <button className="delete" aria-label="delete"></button>
+                </div>
+                <div className="message-body">
+                    Message Body
+                </div>
+            </article>
+        </li>
+        <li><a id="imageBox" draggable="true" ondragstart="drag(event)">Image Box</a>
+            <figure className="image is-128x128"  draggable="true" ondragstart="drag(event)">
+                <img src=""></img>
+            </figure>
+        </li>
+        <li><a id="hero" draggable="true" ondragstart="drag(event)">Hero</a>
+            <section className="hero"  draggable="true" ondragstart="drag(event)">
+                <div className="hero-body">
+                    <div className="container">
+                        <h1 className="title">
+                            Hero title
+                        </h1>
+                        <h2 className="subtitle">
+                            Hero subtitle
+                        </h2>
                     </div>
-                </li>
-                <li><a id="container" draggable="true" ondragstart="drag(event)">Container</a>
-                    <div class="container">
-                        <div class="notification">
-                            This container is <strong>centered</strong> on desktop.
-                    </div>
-                    </div>
-                </li>
-                <li><a id="footer" draggable="true" ondragstart="drag(event)">Footer</a>
-                    <footer class="footer">
-                        <div class="content has-text-centered">
-                            <p>                            
-                            </p>
-                        </div>
-                    </footer>
-                </li>
-            </ul>
-        </aside>
+                </div>
+            </section>
+        </li>
+        <li><a id="form" draggable="true" ondragstart="drag(event)">Form</a>
+            <div className="field"  draggable="true" ondragstart="drag(event)">
+                <label className="label">Name</label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="e.g Alex Smith"></input>
+                </div>
+            </div>
+        </li>
+        <li><a id="container" draggable="true" ondragstart="drag(event)">Container</a>
+            <div className="container"  draggable="true" ondragstart="drag(event)">
+                <div className="notification">
+                    This container is <strong>centered</strong> on desktop.
+            </div>
+            </div>
+        </li>
+        <li><a id="footer" draggable="true" ondragstart="drag(event)">Footer</a>
+            <footer className="footer"  draggable="true" ondragstart="drag(event)">
+                <div className="content has-text-centered">
+                    <p>                            
+                    </p>
+                </div>
+            </footer>
+        </li>
+    </ul>
+</aside>
+
     );
 }
 
