bb55e699f629cd092d1c1616afa2ac2dce435fb2
Britta <polished.athena@gmail.com>
2019-07-18 19:18:49 -0500

    updated hamburger

---
+++ Visual/hamburger.js
@@ -0,0 +1,27 @@
+const auth = require("../../auth");
+auth.initialize();
+
+// $(document).ready(function() {
+
+//     // Check for click events on the navbar burger icon
+//     $(".navbar-burger").click(function() {
+  
+//         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+//         $(".navbar-burger").toggleClass("is-active");
+//         $(".navbar-menu").toggleClass("is-active");
+  
+//     });
+//   });
+
+const auth = require("../../auth");
+auth.initialize();
+
+$(".navbar-burger").click(() => {
+ if ($(".navbar-menu").css("display") == "block") {
+   console.log("close menu");
+   $(".navbar-menu").css("display", "none");
+ } else {
+   console.log("open menu");
+   $(".navbar-menu").css("display", "block");
+ }
+});
