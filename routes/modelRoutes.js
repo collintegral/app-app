01a6d2cb9ce77d7265764018c9b07175d2d4414f
Collin <collintegral@gmail.com>
2019-07-25 19:13:59 -0500

    minor updating

--- routes/modelRoutes.js
+++ routes/modelRoutes.js
@@ -1,11 +1,12 @@
-const db = require("../models");
+const passport = require('passport');
+const db = require('../models');
 
 module.exports = (app) => {
     const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
     // Get all contents of one model
-    app.get("/api/listall/:request", (req, res) => {
-        if (req.params.request !== "User") {
+    app.get('/api/listall/:request', (req, res) => {
+        if (req.params.request !== 'User') {
             db[req.params.request].findAll({}).then(result => {
                 res.json(result);
             });
@@ -19,31 +20,27 @@ module.exports = (app) => {
         })
     );
 
-    app.post("/signup", (req, res) => {
-        if (req.body.email === "" || req.body.username === "" || req.body.password === "" || req.body.confpassword === "") {
-            return "errblank";
+    app.post('/signup', (req, res) => {
+        if (req.body.email === '' || req.body.username === '' || req.body.password === '' || req.body.confpassword === '') {
+            return 'errblank';
         }
-        else if (!emailRegex.test(req.body.email)) {
-            return "errinvemail";
+        if (!emailRegex.test(req.body.email)) {
+            return 'errinvemail';
         }
-        else if (req.body.password.includes(" ")) {
-            return "errspace";
+        if (req.body.password.includes(' ')) {
+            return 'errspace';
         }
-        else if (req.body.password != req.body.confpassword) {
-            return "errpasswords";
+        if (req.body.password !== req.body.confpassword) {
+            return 'errpasswords';
         }
-        else if (req.body.password.trim().length < 6) {
-            return "errpasswordlength";
+        if (req.body.password.trim().length < 6) {
+            return 'errpasswordlength';
         }
-        else {
-            db.User.findOrCreate({ email: req.body.email }, req.body, (err, click, created) => {
-                if (created) {
-                    return "success";
-                }
-                else {
-                    return "erralreadyused";
-                }
-            });
-        }
+        db.User.findOrCreate({ email: req.body.email }, req.body, (err, click, created) => {
+            if (created) {
+                return 'success';
+            }
+                return 'erralreadyused';
+        });
     });
 }
