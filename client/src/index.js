f67a79497e7791d88cf4716ac6bbcf4aba415a01
Collin Christensen <31457823+collintegral@users.noreply.github.com>
2019-07-27 12:32:51 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.css';
-import App from './app.js';
+import App from 'app.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
