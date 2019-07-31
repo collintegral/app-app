09c293708cc29dc7663326cdda45608c80d8c571
Collin <collintegral@gmail.com>
2019-07-30 19:59:19 -0500

    more improvements to schemas

--- models/tabs.js
+++ models/tabs.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Component = require("./components.js");
+const Component = require('./components.js');
 
 const TabSchema = new Schema({
    tabName: {
-       type: Text
+       type: String
        , required: true
    }
    , tabContents: [db.Component]
 });
 
-const Tab = mongoose.model("Tab", TabSchema);
+const Tab = mongoose.model('Tab', TabSchema);
 module.export = Tab;
