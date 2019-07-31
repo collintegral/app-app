56610b4aa2eea856e549b1e7113fe1101d24550e
Collin <collintegral@gmail.com>
2019-07-30 19:14:17 -0500

    getting Heroku to use yarn

--- models/users.js
+++ models/users.js
@@ -1,36 +1,36 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
-const uniqueValidator = require("mongoose-unique-validator");
-const bcrypt = require("bcrypt");
-const findOrCreate = require("mongoose-findorcreate");
+const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
+const findOrCreate = require('mongoose-findorcreate');
+const Page = require('./pages.js');
 
 const UserSchema = new Schema({
-    username: {
-        type: String
-        , required: true
-    }
-    , email: {
-        type: String
-        , required: true
-        , unique: true
-    }
-    , passwordHash: {
-        type: String
-        , required: true
-    }
-    , children: [Page]
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  children: [Page],
 });
 
 UserSchema.plugin(uniqueValidator);
 UserSchema.plugin(findOrCreate);
 
-UserSchema.methods.validPassword = password => {
-    return bcrypt.compareSync(password, this.passwordHash);
-}
+UserSchema.methods.validPassword = password => bcrypt.compareSync(password, this.passwordHash);
 
-UserSchema.virtual("password").set(value => {
-    this.passwordHash = bcrypt.hashSync(value, 15)
-})
+UserSchema.virtual('password').set((value) => {
+  this.passwordHash = bcrypt.hashSync(value, 15);
+});
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema);
 module.export = User;
