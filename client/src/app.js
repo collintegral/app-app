30eb4c5679ab3c712ba5ce7927d47980f4d25c73
Britta Hale <polished.athena@gmail.com>
2019-08-02 19:26:29 -0500

    working on grid

--- client/src/app.js
+++ client/src/app.js
@@ -21,27 +21,33 @@ export default function CenteredGrid() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>m=4</Paper>
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>m=4</Paper>
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>m=4</Paper>
         </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>m=4</Paper>
         </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>m=4</Paper>
         </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>m=4</Paper>
         </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>m=4</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>m=4</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>m=4</Paper>
         </Grid>
       </Grid>
     </div>
